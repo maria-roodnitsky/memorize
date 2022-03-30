29fbb567f29bcdb5ef5735593873adb1d0e8899e
Maria Roodnitsky <maria.roodnitsky@gmail.com>
2022-03-30 10:22:17 -0700

    Lecture 1 Code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,12 +7,80 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let emojis = ["🌲", "🙂", "🥰", "😇", "🌺"]
+    @State var emojiCount = 4
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                deleteButton
+                Spacer()
+                addButton
+            }
+        }
+        .padding(.horizontal)
+        .font(.largeTitle)
+    }
+    
+    var addButton: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+            emojiCount += 1
+            }
+        }, label: {
+                Image(systemName: "plus.circle")
+            })
+    }
+    
+    var deleteButton: some View {
+        Button(action: {
+            if emojiCount > 1 {
+            emojiCount -= 1
+            }
+            
+        }, label: {
+                Image(systemName: "minus.circle")
+            })
     }
 }
+
+
+
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack{
+            let cardShape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                cardShape.foregroundColor(.white)
+                cardShape.strokeBorder(lineWidth: 3)
+                Text(content)
+            } else {
+                cardShape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
